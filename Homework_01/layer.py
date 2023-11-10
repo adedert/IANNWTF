c6ede473de84600964f1256133f402dd1bbb140b
adedert <anne.dedert@googlemail.com>
2023-11-10 17:06:05 +0100

    Final Upload

---
+++ Homework_01/layer.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class Layer():
+
+    def __init__(self, input_size: int, num_units: int, activation):
+        self.activation = activation
+        self.num_units = num_units
+        self.input_size = input_size
+        self.weights = np.random.normal(loc = 0, scale = 0.2, size = (input_size, num_units))
+        self.bias = np.zeros((1, num_units))
+        self.prev_layer_output = None
+
+    def forward(self, input):
+        self.pre_activation = np.dot(input, self.weights) + self.bias
+        self.post_activation = self.activation(self.pre_activation)
+        return self.post_activation
+
+    def weights_backwards(self, output, gradients):
+        self.d_weights = np.dot(output.T, gradients)
+        self.d_bias = np.mean(gradients, axis=0)
+    
+    def activation_backwards(self, output, dhidden):
+        return self.activation.backwards(output, dhidden)
+    
+    def update(self, learning_rate):
+        self.weights -= learning_rate * self.d_weights
+        self.bias -= learning_rate * self.d_bias 
+    
+
+    def __repr__(self):
+       return f"Layer(num_units={self.num_units}, input_size={self.input_size}, activation={self.activation})"
---
+++ Homework_01/mlp.py
@@ -0,0 +1,44 @@
+import numpy as np
+from layer import Layer
+from activations import Sigmoid, Softmax
+
+class MLP():
+
+    def __init__(self, input_size: int, layer_sizes: list):
+        self.layers = []
+        layer_sizes = [input_size] + layer_sizes
+        for i in range(len(layer_sizes) - 1):
+            #softmax activation for last layer, otherwise Sigmoid
+            if i == len(layer_sizes) - 2:
+                activation = Softmax()
+            else:
+                activation = Sigmoid()
+            self.layers.append(Layer(layer_sizes[i], layer_sizes[i + 1], activation))
+        self.info = print("\n".join([str(layer) for layer in self.layers]))
+
+    def forward(self, input):
+      output = input
+      for layer in self.layers:
+          layer.prev_layer_output = output
+          output = layer.forward(output)
+      return output
+    
+    def backwards(self, grad):
+        for l in reversed(self.layers):
+            if l == self.layers[0]:
+                grad = l.activation.backwards(l.post_activation, grad)
+                l.weights_backwards(l.prev_layer_output, grad)
+            else:
+                if l == self.layers[-1]:
+                    l.weights_backwards(l.prev_layer_output, grad)
+                    grad = np.dot(l.weights, grad.T)
+                else:
+                    grad = l.activation.backwards(l.post_activation, grad)
+                    l.weights_backwards(l.prev_layer_output, grad)
+                    grad = np.dot(l.weights, grad.T)
+
+    def update(self, learning_rate):
+        for l in self.layers:
+            l.update(learning_rate)
+            
+    
